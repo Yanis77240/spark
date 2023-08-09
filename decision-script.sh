039d04fc249c54ea6f27cfac52a78e4fdcf2a0c5
SteBaum <stephan.baum@edu.dsti.insitute>
2023-08-09 12:00:30 +0200

    correctiion attempt 2

--- decision-script.sh
+++ decision-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$(mysql -h 10.100.99.143 -u $user -p$pass SPARK2 -e 'SELECT @passed;' -sN)" = 0 ]; then
+if [ "$(mysql -h 10.100.99.143 -u $user -p$pass SPARK2 -e 'SELECT CASE WHEN EXISTS (SELECT 1 FROM comparison_${number}) THEN 0 ELSE 1 END INTO @passed; SELECT @passed;' -sN)" = 0 ]; then
     mysql -h 10.100.99.143 -u $user -p$pass SPARK2 -e "DROP TABLE test_list_${number}, aborted_tests_${number}, results_${number}, comparison_${number};"
     echo "Pipeline terminated due to an increase of failed tests." >&2
     exit 1
