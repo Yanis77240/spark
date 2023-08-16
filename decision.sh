8cd152593f8d09c99fd4f013e8095920f5f0aeea
SteBaum <stephan.baum@edu.dsti.insitute>
2023-08-16 14:30:15 +0200

    corrected decision.sh

--- decision.sh
+++ decision.sh
@@ -3,14 +3,13 @@
 # If the file comparison.json is empty, it means that no new errors appear compared to the previous run and the pipeline stops, otherwise everuthing is ok.
 
 build_number=$1  # First argument passed to the script
-comparison_file=$2 # Second argument passed to the script
 
 file="comparison.csv"
 
 if [ -s "$file" ]; then # if the file is not empty
     echo "There are new errors compared to the comparison run."
     # we send the list with the additional errors to the nexus repository
-    curl -v -u $user:$pass --upload-file "$file" http://10.10.10.11:30000/repository/scala-test-comparison/spark/comparison-"$build_number"-"$comparison_number"
+    curl -v -u $user:$pass --upload-file "$file" http://10.10.10.11:30000/repository/scala-test-comparison/spark/comparison-"$build_number".json
     exit 1  # Exit with an error code
 else
     echo "No new errors in the tests."
